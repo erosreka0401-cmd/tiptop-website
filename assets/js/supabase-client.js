a55ce7d072b2a36e86874fd9af07d40ac7863724
PéBé <eros.reka0401@gmail.com>
2026-06-22 03:50:08 +0200

    +PLACEHOLDER

--- assets/js/supabase-client.js
+++ assets/js/supabase-client.js
@@ -1,6 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "https://wcfszajjrbnatabefnfh.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_2DhrMmzUEsb3q-P9JRzrhQ_tVl7iJ1z";
+const SUPABASE_URL = "https://wcfszajjrbnatabefnfh.supabase.co/rest/v1/";
+const SUPABASE_PUBLISH_KEY = "sb_publishable_2DhrMmzUEsb3q-P9JRzrhQ_tVl7iJ1z";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
+const SUPABASE_PROJECT_URL = SUPABASE_URL.replace(/\/rest\/v1\/?$/, "");
+
+export const supabase = createClient(SUPABASE_PROJECT_URL, SUPABASE_PUBLISH_KEY);
